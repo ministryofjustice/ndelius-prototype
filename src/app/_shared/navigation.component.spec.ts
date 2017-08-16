d955d59a5107eb21ddcc98f30519c16627cb1397
Paul Massey <paul.massey@scriptwerx.io>
2017-08-16 16:00:11 +0100

    [PM]: Fixed lint.

--- src/app/_shared/navigation.component.spec.ts
+++ src/app/_shared/navigation.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from "@angular/router/testing";
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NavigationComponent } from './navigation.component';
 
