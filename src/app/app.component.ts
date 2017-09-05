d9ece5cbe11b6515d15770edfb46096ab791261b
Paul Massey <paul.massey@scriptwerx.io>
2017-09-05 15:28:06 +0100

    [PM]: Corrected State interface.
    Included state debug code in app.component.ts
    Due to lint issues, rolled back to:
    
    @angular/cli: 1.3.2
    
    @angular/animations: 4.4.0-RC.0
    @angular/common: 4.4.0-RC.0
    @angular/compiler: 4.4.0-RC.0
    @angular/core: 4.4.0-RC.0
    @angular/forms: 4.4.0-RC.0
    @angular/http: 4.4.0-RC.0
    @angular/platform-browser: 4.4.0-RC.0
    @angular/platform-browser-dynamic: 4.4.0-RC.0
    @angular/router: 4.4.0-RC.0
    @angular/compiler-cli: 4.4.0-RC.0
    @angular/language-service: 4.4.0-RC.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
+import { Store } from '@ngrx/store';
+
+import { getCurrentState, IState } from './_shared/reducer/state.reducers';
+
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
@@ -16,13 +20,23 @@ declare const jquery: any;
 export class AppComponent implements OnInit {
 
   /**
-   *
+   * @constructor
    * @param {Router} router
    * @param {ActivatedRoute} activatedRoute
    * @param {Title} titleService
+   * @param {Store<IState>} store
    */
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private titleService: Title) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private titleService: Title, private store: Store<IState>) {
     // Empty
+  }
+
+  /**
+   * @FIXME: Remove this.
+   */
+  private checkState() {
+    this.store.select(getCurrentState).subscribe(state => {
+      console['info']('Updated state:', state);
+    });
   }
 
   /**
@@ -47,6 +61,7 @@ export class AppComponent implements OnInit {
         this.titleService.setTitle(event['title'] + pathSuffix);
         $.getScript('assets/javascripts/application.js');
         window.scrollTo(0, 0);
+        this.checkState(); // @FIXME: Remove this
       });
   }
 }
