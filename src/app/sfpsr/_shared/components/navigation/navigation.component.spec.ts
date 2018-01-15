8d49d844b02697a68b4a4a6dcaa6a1f7d949c440
Paul Massey <paul.massey@scriptwerx.io>
2018-01-15 13:53:02 +0000

    Release v1.5.8
    Updated navigation

--- src/app/sfpsr/_shared/components/navigation/navigation.component.spec.ts
+++ src/app/sfpsr/_shared/components/navigation/navigation.component.spec.ts
@@ -35,7 +35,7 @@ describe('Component: Navigation (shared)', () => {
   });
 
   it('should render the template', () => {
-    expect(compiled.querySelector('li').innerHTML).toBe('Offender details');
+    expect(compiled.querySelector('.sub-sub-item').innerHTML).toBe('- Save draft report');
   });
 
 });
