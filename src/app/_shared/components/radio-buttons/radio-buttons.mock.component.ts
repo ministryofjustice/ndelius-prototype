0efed9811c3cc727d0880cf204516ae9046699fa
Paul Massey <paul.massey@scriptwerx.io>
2018-05-23 14:06:26 +0100

    Lint

--- src/app/_shared/components/radio-buttons/radio-buttons.mock.component.ts
+++ src/app/_shared/components/radio-buttons/radio-buttons.mock.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RadioButtonsComponent } from './radio-buttons.component';
 
 @Component({
   selector: 'app-radio-buttons',
   template: ''
 })
-export class MockRadioButtonsComponent extends RadioButtonsComponent {
+export class MockRadioButtonsComponent extends RadioButtonsComponent implements OnInit {
   ngOnInit() {
     // OVERRIDE
   }
