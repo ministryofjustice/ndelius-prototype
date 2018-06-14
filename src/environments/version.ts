93b4f307cddb88292d6e3e2687fedfd7b13a2c58
Paul Massey <paul.massey@scriptwerx.io>
2018-06-14 15:00:04 +0100

    Included version on main start screen

---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "53be663-dirty",
+    "hash": "53be663",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "53be663-dirty",
+    "semverString": null,
+    "version": "1.8.0-beta.3"
+};
+/* tslint:enable */
