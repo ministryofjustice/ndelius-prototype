d69787daf91d5bd6b965bd2fd80c7944301abaf0
Paul Massey <paul.massey@scriptwerx.io>
2018-06-21 15:55:30 +0100

    Updated stub to include risk data

--- src/app/offender-summary/offender-details/components/offender-risk/offender-risk.component.ts
+++ src/app/offender-summary/offender-details/components/offender-risk/offender-risk.component.ts
@@ -6,41 +6,6 @@ import { Component, Input } from '@angular/core';
 })
 export class OffenderRiskComponent {
 
-  @Input() public data: any;
-
-  riskData = [
-    {
-      level: 'high',
-      type: 'Safeguarding',
-      details: 'Weapons',
-      date: '21/11/2017',
-      information: 'No data',
-      active: false
-    },
-    {
-      level: 'medium',
-      type: 'Public protection',
-      details: 'Domestic violence',
-      date: '18/10/2016',
-      information: 'No data',
-      active: false
-    },
-    {
-      level: 'low',
-      type: 'RoSH',
-      details: 'Children',
-      date: '03/09/2015',
-      information: 'No data',
-      active: false
-    },
-    {
-      level: 'very high',
-      type: 'Information',
-      details: 'Home Office Interest',
-      date: '21/11/2017',
-      information: 'No data',
-      active: false
-    }
-  ];
+  @Input() public data = [];
 
 }
