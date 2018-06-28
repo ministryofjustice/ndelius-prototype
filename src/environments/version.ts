0d18305f06346a8845ef27bf91a6b8f87b5db647
Paul Massey <paul.massey@scriptwerx.io>
2018-06-28 10:49:57 +0100

    Bump version

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,14 +1,14 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "6235549-dirty",
-    "hash": "6235549",
+    "dirty": false,
+    "raw": "c918545",
+    "hash": "c918545",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "6235549-dirty",
+    "suffix": "c918545",
     "semverString": null,
-    "version": "1.9.2"
+    "version": "1.9.3"
 };
 /* tslint:enable */
