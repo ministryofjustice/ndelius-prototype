cfc4dbb12a04dfd05541e5d87f86c0daae58b579
Paul Massey <paul.massey@scriptwerx.io>
2018-08-02 13:39:59 +0100

    Updated Parole report as per #45

--- src/app/parom1/current-risk-assessment/model/current-risk-assessment.model.ts
+++ src/app/parom1/current-risk-assessment/model/current-risk-assessment.model.ts
@@ -3,7 +3,9 @@ export interface IRiskAssessment {
   ogrs3Percentage: string;
   ogpProbability: string;
   ovpProbability: string;
+  riskMatrix2000Completed: string;
   riskMatrix2000: string;
+  saraCompleted: string;
   sara: string;
   saved: boolean;
   valid: boolean;
