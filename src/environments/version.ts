a765d45f2464840f24b30b3e6bbeffce3b561f7e
Paul Massey <paul.massey@scriptwerx.io>
2018-10-11 08:53:19 +0100

    Fixed build

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,13 +2,13 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "0f20fb9-dirty",
-    "hash": "0f20fb9",
+    "raw": "e28153b-dirty",
+    "hash": "e28153b",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "0f20fb9-dirty",
+    "suffix": "e28153b-dirty",
     "semverString": null,
-    "version": "2.1.0"
+    "version": "2.1.1"
 };
 /* tslint:enable */
